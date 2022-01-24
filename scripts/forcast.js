a5122df1c63003981a3877f4fcdd4ec756fc4698
Aka Dev <Akadev404@gmail.com>
2022-01-25 01:35:03 +0300

    first commit

---
+++ scripts/forcast.js
@@ -0,0 +1,21 @@
+const key = 'Z1K3x6pGuGQm8rAt2zpSnEcENL5aCZDz';
+
+// get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+
+// get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
